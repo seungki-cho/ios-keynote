a59ac2fe4e4384801b562cb16b909c8aa2352d84
cho seungki <choseungki@choui-MacBookPro.local>
2023-07-17 13:54:42 +0900

    feat: Square - 정사각형 클래스 생성

--- MyKeynote/MyKeynote/Model/Square.swift
+++ MyKeynote/MyKeynote/Model/Square.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+class Square: Rect, CustomStringConvertible {
+    let id: String
+    private(set) var height: Double
+    private(set) var color: SKColor
+    var description: String { "(\(id)), Height:\(height), \(color.description)" }
+    
+    init(id: String, color: SKColor, height: Double) {
+        self.id = id
+        self.color = color
+        self.height = height
+    }
+}
