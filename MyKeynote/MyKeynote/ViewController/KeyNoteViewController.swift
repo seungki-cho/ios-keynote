01f24a1b272279858978af5ac9b5f2c47d0c222b
cho seungki <choseungki@choui-MacBookPro.local>
2023-07-17 13:51:44 +0900

    chore: KeyNoteVC - 뷰컨트롤러 이름 수정

--- MyKeynote/MyKeynote/ViewController/KeyNoteViewController.swift
+++ MyKeynote/MyKeynote/ViewController/KeyNoteViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  KeyNoteViewController.swift
 //  MyKeynote
 //
 //  Created by cho seungki on 2023/07/17.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class KeyNoteViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
