8c736f0db83dbc43d6770baf6f86d4acfe8862d1
cho seungki <choseungki@choui-MacBookPro.local>
2023-07-17 16:39:16 +0900

    chore: OneToTen - SKColor - alpha - 프로퍼티 래퍼

--- MyKeynote/MyKeynote/Model/SKColor.swift
+++ MyKeynote/MyKeynote/Model/SKColor.swift
@@ -11,13 +11,13 @@ struct SKColor: CustomStringConvertible {
     let red: UInt8
     let green: UInt8
     let blue: UInt8
-    let alpha: Int
+    @OneToTen private(set) var alpha: Int
     
     init(red: UInt8, green: UInt8, blue: UInt8, alpha: Int) {
         self.red = red
         self.green = green
         self.blue = blue
-        self.alpha = alpha > 10 ? 10 : (alpha < 0 ? 0 : alpha)
+        self.alpha = alpha
     }
     
     var description: String { "R:\(red), G:\(green), B:\(blue), Alpha: \(String(format: "%2d", alpha))" }
--- MyKeynote/MyKeynote/Util/OneToTen.swift
+++ MyKeynote/MyKeynote/Util/OneToTen.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+@propertyWrapper
+struct OneToTen {
+    private var value: Int = 0
+    var wrappedValue: Int {
+        get { value }
+        set {
+            if newValue < 0 {
+                self.value = 0
+            } else if newValue > 10 {
+                self.value = 10
+            } else {
+                self.value = newValue
+            }
+        }
+    }
+    
+    init(wrappedValue iniitalValue: Int) {
+        self.wrappedValue = value
+    }
+}
