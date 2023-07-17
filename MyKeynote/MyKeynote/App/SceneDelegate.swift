dd3ad5555b2ce4207e09f38e55cdf940a04c65a5
cho seungki <choseungki@choui-MacBookPro.local>
2023-07-17 16:39:36 +0900

    chore: SceneDelegate - 초기 ViewController 이름 변경

--- MyKeynote/MyKeynote/App/SceneDelegate.swift
+++ MyKeynote/MyKeynote/App/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: windowScene)
-        let viewController = ViewController()
+        let viewController = KeyNoteViewController()
         window?.rootViewController = viewController
         window?.makeKeyAndVisible()
     }
