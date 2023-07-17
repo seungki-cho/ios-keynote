d4b2103fdf8ed7fae02448837cbb06ed19a41aed
cho seungki <choseungki@192.168.nate.com>
2023-07-17 23:07:32 +0900

    feat: KeyNoteVC - ViewDidLoad - 팩토리 선언 및 로깅

--- MyKeynote/MyKeynote/ViewController/KeyNoteViewController.swift
+++ MyKeynote/MyKeynote/ViewController/KeyNoteViewController.swift
@@ -6,15 +6,24 @@
 //
 
 import UIKit
+import os
 
 class KeyNoteViewController: UIViewController {
-
+    //MARK: - UI Property
+    
+    //MARK: - Property
+    
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
-        // Do any additional setup after loading the view.
+        
+        let factory = RectFactory(idService: IDService.shared, randomNumberGenerator: SystemRandomNumberGenerator())
+        
+        (0..<4).forEach { i in
+            Logger.rectModel.log(level: .debug, "Rect\(i) \(factory.makeSquare().description)")
+        }
     }
-
-
+    //MARK: - Helper
 }
 
