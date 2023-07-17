4ca83e316dfeab06a397a66b83cc3c760ab78d85
cho seungki <choseungki@choui-MacBookPro.local>
2023-07-17 13:54:20 +0900

    feat: SKColor - RGB Alpha 를 포함한 구조체 생성

--- MyKeynote/MyKeynote/Model/SKColor.swift
+++ MyKeynote/MyKeynote/Model/SKColor.swift
@@ -7,3 +7,18 @@
 
 import Foundation
 
+struct SKColor: CustomStringConvertible {
+    let red: UInt8
+    let green: UInt8
+    let blue: UInt8
+    let alpha: Int
+    
+    init(red: UInt8, green: UInt8, blue: UInt8, alpha: Int) {
+        self.red = red
+        self.green = green
+        self.blue = blue
+        self.alpha = alpha > 10 ? 10 : (alpha < 0 ? 0 : alpha)
+    }
+    
+    var description: String { "R:\(red), G:\(green), B:\(blue), Alpha: \(String(format: "%2d", alpha))" }
+}
