28dfed34dd2cd56584e4df49237f1c370ccad62c
cho seungki <choseungki@192.168.nate.com>
2023-07-17 23:05:52 +0900

    feat: RectFactory - makeSquare 구현

--- MyKeynote/MyKeynote/Model/RectFactory.swift
+++ MyKeynote/MyKeynote/Model/RectFactory.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+class RectFactory {
+    private let idService: IDServiceProtocol
+    private var randomNumberGenerator: RandomNumberGenerator
+    
+    init(idService: IDServiceProtocol, randomNumberGenerator: RandomNumberGenerator) {
+        self.idService = idService
+        self.randomNumberGenerator = randomNumberGenerator
+    }
+    
+    func makeSquare() -> Square {
+        Square(id: idService.makeNewID(),
+               color: SKColor(red: UInt8.random(in: 0...255, using: &randomNumberGenerator),
+                              green: UInt8.random(in: 0...255, using: &randomNumberGenerator),
+                              blue: UInt8.random(in: 0...255, using: &randomNumberGenerator),
+                              alpha: Int.random(in: 0...10, using: &randomNumberGenerator)),
+               height: Int.random(in: 100...500, using: &randomNumberGenerator)
+        )
+    }
+}
