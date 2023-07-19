19b69339fa09e46b24ef00854fdd0a504371d063
조승기 <beenzino@kookmin.ac.kr>
2023-07-19 11:12:05 +0900

    feat: ControlStackView - ControlView들을 갖고있는 뷰

--- MyKeynote/MyKeynote/Model/ControlStackView.swift
+++ MyKeynote/MyKeynote/Model/ControlStackView.swift
@@ -5,4 +5,45 @@
 //  Created by 조승기 on 2023/07/18.
 //
 
-import Foundation
+import UIKit
+
+class ControlStackView: UIView {
+    // MARK: - UI properties
+    private let colorControlView = ColorControlView()
+    private let alphaControlView = AlphaControlView()
+    // MARK: - Properties
+    
+    // MARK: - Lifecycles
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureUI()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        configureFrame()
+    }
+    // MARK: - Helpers
+    private func configureUI() {
+        [colorControlView, alphaControlView].forEach {
+            addSubview($0)
+        }
+        backgroundColor = .systemGray5
+    }
+    func configureFrame() {
+        let yMargin = 5.0
+        colorControlView.frame = CGRect(x: 0,
+                                        y: yMargin,
+                                        width: frame.width,
+                                        height: frame.width * 9 / 16)
+        
+        alphaControlView.frame = CGRect(x: 0,
+                                        y: colorControlView.frame.maxY + yMargin,
+                                        width: frame.width,
+                                        height: frame.width * 9 / 16)
+    }
+}
