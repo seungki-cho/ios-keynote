26559f694f2ce210f2fef139e841dd91452c4263
조승기 <beenzino@kookmin.ac.kr>
2023-07-19 11:10:16 +0900

    feat: SlideManager - 배열과 정사각형 추가 메서드

--- MyKeynote/MyKeynote/Model/SlideManager.swift
+++ MyKeynote/MyKeynote/Model/SlideManager.swift
@@ -7,6 +7,22 @@
 
 import Foundation
 
-struct SlideManager {
-    private let rectFactory: RectFactory
+struct SlideManager: SlideManagerProtocol {
+    //MARK: - Dependency
+    private let rectFactory: RectFactoryProtocol
+    
+    //MARK: - Property
+    private var slides: [Rect] = []
+    
+    //MARK: - Lifecycle
+    init(rectFactory: RectFactoryProtocol) {
+        self.rectFactory = rectFactory
+    }
+    
+    //MARK: - Helper
+    mutating func makeSquare() -> Square {
+        let newSquare = rectFactory.makeSquare()
+        slides.append(newSquare)
+        return newSquare
+    }
 }
