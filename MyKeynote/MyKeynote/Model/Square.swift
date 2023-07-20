7553ac5f2ed158b72b08549b20a81fea1a510041
조승기 <beenzino@kookmin.ac.kr>
2023-07-20 23:33:49 +0900

    chore: Square - Rect 상속받도록 변경

--- MyKeynote/MyKeynote/Model/Square.swift
+++ MyKeynote/MyKeynote/Model/Square.swift
@@ -7,22 +7,6 @@
 
 import Foundation
 
-final class Square: Rectable, Colorful {
-    let id: String
-    var height: Int
-    var color: SKColor
-    @OneToTen var alpha: Int
-    
-    var description: String { "(\(id)), Height:\(height), \(color.description), Alpha:\(String(format: "%2d", alpha))" }
-    
-    init(id: String, height: Int, alpha: Int, color: SKColor?, photo: Data? = nil) {
-        self.id = id
-        self.height = height
-        self.alpha = alpha
-        self.color = color ?? SKColor.randomOne()
-    }
-    
-    func changeColor(to color: SKColor) {
-        self.color = color
-    }
+final class Square: Rect {
+    override var aspectRatio: Double { 3 / 4 }
 }
