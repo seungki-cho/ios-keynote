7143f94135f3a1274256c346ed018082cc79741d
조승기 <beenzino@kookmin.ac.kr>
2023-07-20 23:34:27 +0900

    feat: RectFactory - 생성 최대 포인트 지정

--- MyKeynote/MyKeynote/Model/RectFactory.swift
+++ MyKeynote/MyKeynote/Model/RectFactory.swift
@@ -8,6 +8,8 @@
 import Foundation
 
 class RectFactory: RectFactoryProtocol {
+    private var maxPoint = SKPoint(x: 0, y: 0)
+    
     private let idService: IDServiceProtocol
     private var randomNumberGenerator: RandomNumberGenerator
     
@@ -17,6 +19,10 @@ class RectFactory: RectFactoryProtocol {
     }
     
     func make(by type: Rect.Type, photo: Data? = nil) -> Rect {
+    func changeMaxPoint(_ point: SKPoint) {
+        guard point.x > 0 && point.y > 0 else { return }
+        maxPoint = point
+    }
         type.init(id: idService.makeNewID(),
                   height: Int.random(in: 100...500, using: &randomNumberGenerator),
                   alpha: Int.random(in: 0...10, using: &randomNumberGenerator),
