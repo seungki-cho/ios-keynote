374aa40ba208a900517ab8d87d4fee111e064c4f
조승기 <beenzino@kookmin.ac.kr>
2023-07-20 23:28:47 +0900

    chore: Rect - 기존 Square 이동

--- MyKeynote/MyKeynote/Model/Rect.swift
+++ MyKeynote/MyKeynote/Model/Rect.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+class Rect: Rectable, Colorful {
+    let id: String
+    var point: SKPoint
+    var height: Int
+    var color: SKColor
+    @OneToTen var alpha: Int
+    
+    var description: String { "(\(id)), Height:\(height), \(color.description), Alpha:\(String(format: "%2d", alpha))" }
+    
+    required init(id: String, point: SKPoint, height: Int, alpha: Int, color: SKColor?, photo: Data? = nil) {
+        self.id = id
+        self.point = point
+        self.height = height
+        self.alpha = alpha
+        self.color = color ?? SKColor.randomOne()
+    }
+}
