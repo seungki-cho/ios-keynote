2eb9390e2d0a5a353ebc91cdd806d40c52787977
조승기 <beenzino@kookmin.ac.kr>
2023-07-23 17:26:00 +0900

    feat: MockRectFactory - 고정된 값으로 생성하는 목 팩토리 생성

--- MyKeynote/MyKeynoteTests/MockRectFactory.swift
+++ MyKeynote/MyKeynoteTests/MockRectFactory.swift
@@ -8,5 +8,16 @@
 import Foundation
 
 class MockRectFactory: RectFactoryProtocol {
+    func changeMaxPoint(_ point: SKPoint) {
+        
+    }
     
+    func make<T>(by type: T.Type, photo: Data?) -> T where T : Rectable {
+        T.init(id: "x:100, y:100, height:100, alpha:5, color:122,122,122",
+               point: SKPoint(x: 100, y: 100),
+               height: 100,
+               alpha: 5,
+               color: SKColor(red: 122, green: 122, blue: 122),
+               photo: photo)
+    }
 }
--- MyKeynote/MyKeynoteTests/SlideManagerTest.swift
+++ MyKeynote/MyKeynoteTests/SlideManagerTest.swift
@@ -8,32 +8,18 @@
 import XCTest
 
 final class SlideManagerTest: XCTestCase {
-
+    var sut: SlideManager!
+    
     override func setUpWithError() throws {
-        let idService = IDService.shared
-        let randomNumberGenerator = SystemRandomNumberGenerator()
-        
-        let rectFactory = RectFactory(idService: idService, randomNumberGenerator: randomNumberGenerator)
-        let sut = SlideManager(rectFactory: rectFactory)
+        let mockRectFactory = MockRectFactory()
+        sut = SlideManager(rectFactory: mockRectFactory)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
         }
     }
-
 }
