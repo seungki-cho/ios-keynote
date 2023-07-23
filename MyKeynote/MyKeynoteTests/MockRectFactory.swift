17be2287fa1057a6c30eb9fe2c7e7fbf7f6aee1d
조승기 <beenzino@kookmin.ac.kr>
2023-07-23 16:40:57 +0900

    feat: Test - 타겟 추가, MockRectFactory 파일 추가

---
+++ MyKeynote/MyKeynoteTests/MockRectFactory.swift
@@ -0,0 +1,12 @@
+//
+//  MockRectFactory.swift
+//  MyKeynoteTests
+//
+//  Created by 조승기 on 2023/07/22.
+//
+
+import Foundation
+
+class MockRectFactory: RectFactoryProtocol {
+    
+}
--- MyKeynote/MyKeynoteTests/SlideManagerTest.swift
+++ MyKeynote/MyKeynoteTests/SlideManagerTest.swift
@@ -1,17 +1,20 @@
 //
-//  MyKeynoteTests.swift
-//  MyKeynoteTests
+//  SlideManagerTest.swift
+//  SlideManagerTest
 //
 //  Created by cho seungki on 2023/07/17.
 //
 
 import XCTest
-@testable import MyKeynote
 
-final class MyKeynoteTests: XCTestCase {
+final class SlideManagerTest: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let idService = IDService.shared
+        let randomNumberGenerator = SystemRandomNumberGenerator()
+        
+        let rectFactory = RectFactory(idService: idService, randomNumberGenerator: randomNumberGenerator)
+        let sut = SlideManager(rectFactory: rectFactory)
     }
 
     override func tearDownWithError() throws {
