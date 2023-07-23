a3ffe04ee667d72bfd843d61924551632fd6265c
조승기 <beenzino@kookmin.ac.kr>
2023-07-23 17:26:26 +0900

    feat: Test - SlideManager Tap 테스트 추가

--- MyKeynote/MyKeynoteTests/SlideManagerTest.swift
+++ MyKeynote/MyKeynoteTests/SlideManagerTest.swift
@@ -19,6 +19,57 @@ final class SlideManagerTest: XCTestCase {
         sut = nil
     }
 
+    func test_아무것도없는곳Tap하면_선택된Rect가nil이다() {
+        //Given
+        _ = sut.makeRect(by: Square.self)
+        var nilRect: Rectable?
+        sut.selectedRectDidChanged = { rect in
+            nilRect = rect
+        }
+        //When
+        sut.tapped(at: SKPoint(x: 201, y: 201))
+        //Then
+        XCTAssertNil(nilRect)
+    }
+    func test_RectableTap하면_선택된Rect가nil이아니다() {
+        //Given
+        _ = sut.makeRect(by: Square.self)
+        var nilRect: Rectable?
+        sut.selectedRectDidChanged = { rect in
+            nilRect = rect
+        }
+        //When
+        sut.tapped(at: SKPoint(x: 150, y: 150))
+        //Then
+        XCTAssertNotNil(nilRect)
+    }
+    func test_Rectable두번Tap하면_선택된Rectable이일치한다() {
+        //Given
+        let willBeSelectedRect = sut.makeRect(by: Square.self)
+        var selectedRect: Rectable?
+        sut.selectedRectDidChanged = { rect in
+            selectedRect = rect
+        }
+        //When
+        sut.tapped(at: SKPoint(x: 150, y: 150))
+        sut.tapped(at: SKPoint(x: 150, y: 150))
+        //Then
+        XCTAssertNotNil(selectedRect)
+        XCTAssertTrue(willBeSelectedRect.isEqual(with: selectedRect!))
+    }
+    func test_RectableTap후에빈곳Tap하면_선택된Rect가nil이다() {
+        //Given
+        _ = sut.makeRect(by: Square.self)
+        var selectedRect: Rectable?
+        sut.selectedRectDidChanged = { rect in
+            selectedRect = rect
+        }
+        //When
+        sut.tapped(at: SKPoint(x: 150, y: 150))
+        sut.tapped(at: SKPoint(x: 99, y: 99))
+        //Then
+        XCTAssertNil(selectedRect)
+    }
     }
         }
     }
