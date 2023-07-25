80912e9bb8c1d6f7f7d5767ce9647a87bd39a69f
조승기 <beenzino@kookmin.ac.kr>
2023-07-25 17:23:54 +0900

    chore: KeyNoteVC - model과 view 모두에게 알리도록 수정

--- MyKeynote/MyKeynote/ViewController/KeyNoteViewController.swift
+++ MyKeynote/MyKeynote/ViewController/KeyNoteViewController.swift
@@ -166,8 +166,10 @@ extension KeyNoteViewController: UIColorPickerViewControllerDelegate {
         var (red, green, blue): (CGFloat, CGFloat, CGFloat) = (0, 0, 0)
         viewController.selectedColor.getRed(&red, green: &green, blue: &blue, alpha: nil)
         
-        slideManager.changeColor(to: SKColor(red: UInt8(red * 255),
-                                             green: UInt8(green * 255),
-                                             blue: UInt8(blue * 255)))
+        let skColor = SKColor(red: UInt8(red * 255),
+                              green: UInt8(green * 255),
+                              blue: UInt8(blue * 255))
+        slideManager.changeColor(to: skColor)
+        controlStackView.bind(color: skColor)
     }
 }
