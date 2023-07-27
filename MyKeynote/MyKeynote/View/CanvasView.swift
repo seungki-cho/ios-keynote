cb56fe66c6b3d58569af34102ce3595fd992b35f
조승기 <beenzino@kookmin.ac.kr>
2023-07-27 11:36:09 +0900

    feat: CanvasView - Slide 선택 여부 추가

--- MyKeynote/MyKeynote/View/CanvasView.swift
+++ MyKeynote/MyKeynote/View/CanvasView.swift
@@ -13,22 +13,23 @@ protocol CanvasViewDelegate: AnyObject {
 
 class CanvasView: UIView { 
     // MARK: - UI properties
-    private var slide: UIView!
-    
     // MARK: - Properties
     weak var delegate: CanvasViewDelegate?
+    private var slides: [SKID: UIView] = [:]
     
     // MARK: - Lifecycles
     override init(frame: CGRect) {
         super.init(frame: frame)
         configureEvent()
         backgroundColor = .white
+        isHidden = true
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         configureEvent()
         backgroundColor = .white
+        isHidden = true
     }
     // MARK: - Helpers
     private func configureEvent() {
@@ -49,6 +50,7 @@ class CanvasView: UIView {
         slide.backgroundColor = color
         tag = rectable.id.toInt
         addSubview(slide)
+        selectSlide(by: rectable.id)
     }
     
     func changeSelection(to isSelected: Bool) {
@@ -57,9 +59,16 @@ class CanvasView: UIView {
     
     func changeColor(red: CGFloat, green: CGFloat, blue: CGFloat) {
         slide.changeBackgroundColor(red: red, green: green, blue: blue)
+    func selectSlide(by id: SKID) {
+        isHidden = false
+        subviews.forEach { $0.isHidden = true }
+        slides[id]?.isHidden = false
     }
     
     func changeAlpha(to alpha: CGFloat) {
         slide.changeBackgroundAlpha(to: alpha)
+    func deselectSlide() {
+        self.isHidden = true
+        subviews.forEach { $0.isHidden = true }
     }
 }
